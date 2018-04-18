d2c85cb1b85e93e4c44a7c2057d5870c5728e81b
stu201758 <stu201758@4d11ad54-4dd7-4544-aaba-f469505a2a2c>
2018-04-18 20:25:37 +0000

    Followed instructions of exercise 2a) Creating a Eclipse Project and some files, ignoring bin
    git-svn-id: https://build.se.informatik.uni-kiel.de/svn/teaching/40@3 4d11ad54-4dd7-4544-aaba-f469505a2a2c

---
+++ Abgabe01/Aufgabe02/G40S01A02/src/swt/Example.java
@@ -0,0 +1,13 @@
+package swt;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Example {
+	
+	public static void main(String[] tArgs) {
+		
+		System.out.println(StringUtils.join(tArgs,',')); //NOSONAR
+		
+	}
+
+}
