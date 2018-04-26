8251c37e49162f6a944575b9c17695f6d7a933ba
stu201758 <stu201758@4d11ad54-4dd7-4544-aaba-f469505a2a2c>
2018-04-26 09:41:36 +0000

    removed Sprotten
    git-svn-id: https://build.se.informatik.uni-kiel.de/svn/teaching/40@11 4d11ad54-4dd7-4544-aaba-f469505a2a2c

--- Abgabe03/Aufgabe03/src/de/cau/restaurant/speisen/Dosensuppe.java
+++ Abgabe03/Aufgabe03/src/de/cau/restaurant/speisen/Dosensuppe.java
@@ -3,7 +3,7 @@ package de.cau.restaurant.speisen;
 public class Dosensuppe {
 
   private boolean istScharf = true;
-  private boolean mitSojasprossen = true;
+  private boolean mitSojasprossen = false;
     
   private final String beschreibung = "Würzige Suppe mit Sojasprossen";
 
