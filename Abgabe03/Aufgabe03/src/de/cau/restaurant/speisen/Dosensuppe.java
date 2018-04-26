bc404f3d75f17dbe93ed22613666e4037e33eb99
stu201758 <stu201758@4d11ad54-4dd7-4544-aaba-f469505a2a2c>
2018-04-26 09:36:03 +0000

    Added Dosensuppe
    git-svn-id: https://build.se.informatik.uni-kiel.de/svn/teaching/40@9 4d11ad54-4dd7-4544-aaba-f469505a2a2c

---
+++ Abgabe03/Aufgabe03/src/de/cau/restaurant/speisen/Dosensuppe.java
@@ -0,0 +1,10 @@
+package de.cau.restaurant.speisen;
+
+public class Dosensuppe {
+
+  private boolean istScharf = false;
+  private boolean mitSojasprossen = true;
+    
+  private final String beschreibung = "Würzige Suppe mit Sojasprossen";
+
+}
