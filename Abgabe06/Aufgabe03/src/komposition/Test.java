a0c042cb9d77d42b9fb55ec6e0938401e8339d65
stu201758 <stu201758@4d11ad54-4dd7-4544-aaba-f469505a2a2c>
2018-05-17 17:16:50 +0000

    Added test for komposition
    git-svn-id: https://build.se.informatik.uni-kiel.de/svn/teaching/40@32 4d11ad54-4dd7-4544-aaba-f469505a2a2c

--- Abgabe06/Aufgabe03/src/komposition/Test.java
+++ Abgabe06/Aufgabe03/src/komposition/Test.java
@@ -1,10 +1,35 @@
 package komposition;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Lecture l = new Lecture();
+		
+		if(l.isValid()) {
+			System.err.println("Lecture with no Session was valid!");
+		}
+		
+		Session s = new Session(new Date());
+		
+		l.addSession(s);
+		
+		if(!l.isValid()) {
+			System.err.println("Lecture with Session was invalid!");
+		}
+		
+		if(!l.getSessions().contains(s)) {
+			System.err.println("Session add failed!");
+		}
+		
+		l.removeSession(s);
+		
+		if(!l.getSessions().isEmpty()) {
+			System.err.println("Session remove failed!");
+		}
+		
 	}
 
 }
